da0e24df2906c8944fb0300fcba186c44241fc18
Ema Butuza <emabutuza@Emas-MacBook-Air.local>
2025-06-04 07:55:45 +0300

    Remove sem4

--- semester4/MPP/mpp lab/FlowerMarket/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts?(x)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}; 
